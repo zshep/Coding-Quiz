336dbc0f3d6271d4e291854520027fc389515f1d
zsheperdigian <zsheperdigian@gmail.com>
2022-07-27 15:33:33 -0700

    adding more skeleton stuff

--- assets/codingQuiz.js
+++ assets/codingQuiz.js
@@ -1,1 +1,31 @@
-// 
+// list of variable needed for various stuff
+// make object for Questions and Answers 
+var Questions = {
+    question1: "",
+    question2: "",
+    question3: "",
+    question4: "",
+    question5: "",
+    question6: "",
+}
+//To do: Make Function to start game
+    //click button - start game
+
+//To do: Make function to play game
+        //Displays question
+        //Displays Answer Choices
+
+//To do: Make function to work time
+        //  set to count down each second
+
+        // set to lose a chunk of time for wrong answers
+
+
+//To do: Make function for Game over
+        //set text for when time ran out
+        //set text for when all questions are answered
+        //User enter in initials and posts score
+        //save score into local.storage
+        //Show List of high scores (retrieve from local storag
+
+// to do: make function for play again 
