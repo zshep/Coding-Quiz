f8f431dafc3ca5a5eff855020f198327b1b65768
zsheperdigian <zsheperdigian@gmail.com>
2022-07-28 16:07:38 -0700

    re-ordred html to be in three sections, welcome, game, Highscore/redo game

--- assets/codingQuiz.js
+++ assets/codingQuiz.js
@@ -1,4 +1,11 @@
 // list of variable needed for various stuff
+var startBtn = document.getElementById("start_button");
+var optBtn1 = document.querySelector("#option1");
+var optBtn2 = document.querySelector("#option2");
+var optBtn3 = document.querySelector("#option3");
+var optBtn4 = document.querySelector("#option4");
+
+
 // make object for Questions and Answers 
 var Questions = {
     question1: "",
@@ -8,12 +15,14 @@ var Questions = {
     question5: "",
     question6: "",
 }
-//To do: Make Function to start game
-    //click button - start game
 
 //To do: Make function to play game
+function game() {
+        window.alert("HellO");
         //Displays question
         //Displays Answer Choices
+        return;
+}
 
 //To do: Make function to work time
         //  set to count down each second
@@ -29,3 +38,11 @@ var Questions = {
         //Show List of high scores (retrieve from local storag
 
 // to do: make function for play again 
+
+
+
+
+// Action Code:
+
+        //listen for click to start game
+startBtn.addEventListener("click", game);
