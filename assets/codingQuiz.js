04b95b76b4e6a70d2220e6915bb890c672fd6ceb
zsheperdigian <zsheperdigian@gmail.com>
2022-07-29 15:20:47 -0700

    after tutor session, modified my object into an array of objects

--- assets/codingQuiz.js
+++ assets/codingQuiz.js
@@ -4,26 +4,57 @@ var optBtn1 = document.querySelector("#option1");
 var optBtn2 = document.querySelector("#option2");
 var optBtn3 = document.querySelector("#option3");
 var optBtn4 = document.querySelector("#option4");
-
+var quest = document.querySelector("#question")
 
 // make object for Questions and Answers 
-var Questions = {
-    question1: "",
-    question2: "",
-    question3: "",
-    question4: "",
-    question5: "",
-    question6: "",
-}
+var questions = [
+        {
+                prompt: "How are you?",
+                options: ["good", "bad", "terrible", "great"],
+                ans: "good"
+        },
+        {
+                prompt: "Where are you?",
+                options: ["here", "there", "everywhere", "nowhere"],
+                ans: "there"
+        },
+]
+var currentQ;
 
 //To do: Make function to play game
 function game() {
-        window.alert("HellO");
-        //Displays question
+        // window.alert("HellO");
+        console.log("the game has started");
+        //Displays question -grab question and insert into
+        // quest.
+        currentQ = 0;
+        nextQuestion()
+        // timerBegins()
         //Displays Answer Choices
         return;
 }
 
+
+function nextQuestion(){
+        quest.textContent = questions[currentQ].prompt
+        optBtn1.textContent = questions[currentQ].options[0]
+        optBtn2.textContent = questions[currentQ].options[1]
+}
+//TO do: make score counter
+
+function checkAnswer(event) {
+        var userPick = event.target.textContent;
+        if(userPick == questions[currentQ].ans){
+                console.log("right")
+        } else {
+                console.log("wrong")
+        }
+
+        currentQ++
+        if(currentQ< questions.length) {
+                nextQuestion()
+        }
+}
 //To do: Make function to work time
         //  set to count down each second
 
@@ -46,3 +77,5 @@ function game() {
 
         //listen for click to start game
 startBtn.addEventListener("click", game);
+optBtn1.addEventListener("click", checkAnswer);
+optBtn2.addEventListener("click", checkAnswer);
