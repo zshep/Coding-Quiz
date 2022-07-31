49bff573ea8376b856f9713d5bbafaf8f8c9c893
zsheperdigian <zsheperdigian@gmail.com>
2022-07-31 14:46:39 -0700

    added a timer function, to do: make the timer lose an additional 5 seconds

--- assets/codingQuiz.js
+++ assets/codingQuiz.js
@@ -1,10 +1,28 @@
-// list of variable needed for various stuff
+// variables for buttons
 var startBtn = document.getElementById("start_button");
+var restartBtn =document.getElementById("restart_button");
 var optBtn1 = document.querySelector("#option1");
 var optBtn2 = document.querySelector("#option2");
 var optBtn3 = document.querySelector("#option3");
 var optBtn4 = document.querySelector("#option4");
-var quest = document.querySelector("#question")
+
+
+//list of main containers
+var quest = document.querySelector("#question");
+var quizCon = document.querySelector(".Quiz");
+var welcomeCon = document.querySelector(".Welcome");
+var scoresCon = document.querySelector(".High-Scores");
+var timeEl = document.querySelector(".timer");
+var mainEl = document.querySelector(".main");
+var headerCon = document.querySelector(".header");
+
+//setting defualt hidden/visisble
+welcomeCon.style.display = "block";
+quizCon.style.display = "none";
+scoresCon.style.display = "none";
+headerCon.style.display = "none";
+
+var secondsLeft = 10;
 
 // make object for Questions and Answers 
 var questions = [
@@ -30,13 +48,18 @@ var currentQ;
 
 //To do: Make function to play game
 function game() {
-        
-        console.log("the game has started");
-        //Displays question -grab question and insert into
-       
+         console.log("the game has started");
+        timeEl.content = `Time Remaining: + ${secondsLeft}`;
+        // change display for welcome to hidden and quiz to visible
+        welcomeCon.style.display = "none";
+        quizCon.style.display = "block";
+        headerCon.style.display = "block";
+
+        //Sets currentQ to first question in array of questions      
         currentQ = 0;
         console.log(currentQ);
-        // timerBegins()
+        timer();
+
         //displays question and answer choices
         nextQuestion();
         
@@ -82,20 +105,47 @@ function checkAnswer(event) {
 }
 // function to work time
 function timer() {
+           
+               
+         console.log("Timer has started")
+        
+          var timerInterval = setInterval(function() {
+           
+            secondsLeft--;
+            timeEl.textContent = `Time Remaining:${secondsLeft}`;;
+        
+            if(secondsLeft === 0) {
+              clearInterval(timerInterval);
+              console.log("The timer has ended");
+              gameOver();
+            }
+          }, 1000);
 
-        //  set to count down each second
 
         // set to lose a chunk of time for wrong answers
-        return;
-}
 
-//To do: Make function for Game over
+        }       
+       
+
+
+// function for Game over
+function gameOver(){
+        
+        //change visibility for three containers
+        welcomeCon.style.display = "none";
+        quizCon.style.display = "none";
+        scoresCon.style.display = "block";
+        headerCon.style.display = "none";
+
         //set text for when time ran out
         //set text for when all questions are answered
+       
         //User enter in initials and posts score
         //save score into local.storage
         //Show List of high scores (retrieve from local storag
 
+
+}
 // to do: make function for play again 
 
 
@@ -105,7 +155,10 @@ function timer() {
 
         //listen for click to start game
 startBtn.addEventListener("click", game);
+restartBtn.addEventListener("click", game)
 optBtn1.addEventListener("click", checkAnswer);
 optBtn2.addEventListener("click", checkAnswer);
 optBtn3.addEventListener("click", checkAnswer);
 optBtn4.addEventListener("click", checkAnswer);
+
+// add listen event for the input
